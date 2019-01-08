a090396b5374683eab52de16ae30ac884f98c165
unknown <kumard2312@gmail.com>
2019-01-09 00:48:29 +0530

    v196

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-import {Route,Link} from 'react-router-dom';
+import {Route,NavLink,Switch} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     
@@ -19,18 +20,26 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink  to="/" exact
+                            activeClassName="my-active"
+                            activeStyle={{
+                                color:'#fa923f',
+                                textDecoration:'underline'
+                            }}
+                            >Home</NavLink ></li>
+                            <li><NavLink  to={{
                                 pathname:'/new-post',
                                 hash:'#submit',
                                 search:'?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink ></li>
                         </ul>
                     </nav>
                 </header>
-
+            <Switch>
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" exact component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
+            </Switch>
             </div>
         )
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import axios from 'axios';
 import axios from '../../../axios';
+import {Link} from 'react-router-dom';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
 
@@ -36,11 +37,14 @@ import './Posts.css';
     let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
     if (!this.state.error) {
         posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
+            return (<Link to={'/'+post.id}  key={post.id}  >
+            <Post 
+               
                 title={post.title} 
                 author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} />;
+                clicked={() => this.postSelectedHandler(post.id)} />
+                </Link>
+            );
         });
     }
     return (
